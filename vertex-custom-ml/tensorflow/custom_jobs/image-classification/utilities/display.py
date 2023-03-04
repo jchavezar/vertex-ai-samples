0f98d74dd443f957b728f65b4b58e18f816f7b12
jchavezar <jchavezar@gmail.com>
2023-03-03 23:17:51 -0500

    mar-3-23

--- vertex-custom-ml/tensorflow/custom_jobs/image-classification/utilities/display.py
+++ vertex-custom-ml/tensorflow/custom_jobs/image-classification/utilities/display.py
@@ -1,5 +1,75 @@
+import numpy as np
 from matplotlib import pyplot as plt
+
+CLASSES = ['daisy', 'dandelion', 'roses', 'sunflowers', 'tulips'] 
+
 def training_curves(training, validation, title, subplot):
+  if subplot%10==1: # set up the subplots on the first call
+    plt.subplots(figsize=(10,10), facecolor='#F0F0F0')
+    #plt.tight_layout()
+  ax = plt.subplot(subplot)
+  ax.set_facecolor('#F8F8F8')
+  ax.plot(training)
+  ax.plot(validation)
+  ax.set_title('model '+ title)
+  ax.set_ylabel(title)
+  ax.set_xlabel('epoch')
+  ax.legend(['train', 'valid.'])
+
+
+def dataset_to_numpy_util(dataset, N):
+  dataset = dataset.batch(N)
+  
+  # In eager mode, iterate in the Datset directly.
+  for images, labels in dataset:
+    numpy_images = images.numpy()
+    numpy_labels = labels.numpy()
+    break;
+
+  return numpy_images, numpy_labels
+
+def title_from_label_and_target(label, correct_label):
+  label = np.argmax(label, axis=-1)  # one-hot to class number
+  correct_label = np.argmax(correct_label, axis=-1) # one-hot to class number
+  correct = (label == correct_label)
+  return "{} [{}{}{}]".format(CLASSES[label], str(correct), ', shoud be ' if not correct else '',
+                              CLASSES[correct_label] if not correct else ''), correct
+
+def display_one_flower(image, title, subplot, red=False):
+    plt.subplot(subplot)
+    plt.axis('off')
+    plt.imshow(image)
+    plt.title(title, fontsize=16, color='red' if red else 'black')
+    return subplot+1
+  
+def display_9_images_from_dataset(dataset):
+  subplot=331
+  plt.figure(figsize=(13,13))
+  images, labels = dataset_to_numpy_util(dataset, 9)
+  for i, image in enumerate(images):
+    title = CLASSES[np.argmax(labels[i], axis=-1)]
+    subplot = display_one_flower(image, title, subplot)
+    if i >= 8:
+      break;
+              
+  #plt.tight_layout()
+  plt.subplots_adjust(wspace=0.1, hspace=0.1)
+  plt.show()
+  
+def display_9_images_with_predictions(images, predictions, labels):
+  subplot=331
+  plt.figure(figsize=(13,13))
+  for i, image in enumerate(images):
+    title, correct = title_from_label_and_target(predictions[i], labels[i])
+    subplot = display_one_flower(image, title, subplot, not correct)
+    if i >= 8:
+      break;
+              
+  #plt.tight_layout()
+  plt.subplots_adjust(wspace=0.1, hspace=0.1)
+  plt.show()
+  
+def display_training_curves(training, validation, title, subplot):
   if subplot%10==1: # set up the subplots on the first call
     plt.subplots(figsize=(10,10), facecolor='#F0F0F0')
     #plt.tight_layout()
