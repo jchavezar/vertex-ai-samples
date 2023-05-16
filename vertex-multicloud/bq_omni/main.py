d0aded3d557e25e83f1c57ea9876c5d5e1a146a6
jchavezar <jchavezar@gmail.com>
2023-05-16 00:33:48 +0000

    may-15-23

--- vertex-multicloud/bq_omni/main.py
+++ vertex-multicloud/bq_omni/main.py
@@ -75,5 +75,22 @@ eval_df.head()
 from sklearn.metrics import accuracy_score, f1_score
 result = tabular_model.evaluate(eval_df)
 
-tabular_model.predict(eval_df)
+#%%
+pred = tabular_model.predict(eval_df)
+
+def print_metrics(y_true, y_pred, tag):
+    if isinstance(y_true, pd.DataFrame) or isinstance(y_true, pd.Series):
+        y_true = y_true.values
+    if isinstance(y_pred, pd.DataFrame) or isinstance(y_pred, pd.Series):
+        y_pred = y_pred.values
+    if y_true.ndim>1:
+        y_true=y_true.ravel()
+    if y_pred.ndim>1:
+        y_pred=y_pred.ravel()
+    val_acc = accuracy_score(y_true, y_pred)
+    val_f1 = f1_score(y_true, y_pred)
+    print(f"{tag} Acc: {val_acc} | {tag} F1: {val_f1}")
+
+print_metrics(eval_df['label'], pred["prediction"], tag="Holdout")
+
 # %%
