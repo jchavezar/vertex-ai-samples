eee40af97f062b16eba7b54a489e294361b7df43
jchavezar <jchavezar@gmail.com>
2023-08-11 12:03:15 -0400

    aug-7-23

--- gen_ai/bigquery_llm/bq_llm.py
+++ gen_ai/bigquery_llm/bq_llm.py
@@ -11,27 +11,27 @@ project="vtxdemos"
 dataset="custom"
 table="delo"
 
+parameters = {
+    "temperature": 0.2,
+    "max_output_tokens": 1024
+}
 
 def llm_code_gen(
         prompt,
         project=project, 
         dataset=dataset, 
-        table=table):
+        table=table,
+        parameters=parameters):
     aiplatform.init(project="vtxdemos", location="us-central1")
     
-    parameters = {
-        "temperature": 0.2,
-        "max_output_tokens": 1024
-    }
-
     model = CodeGenerationModel.from_pretrained("code-bison@001")
     response = model.predict(
         prefix = f"""Context:
     You are a GoogleSQL expert. Given an input question, first create a syntactically correct GoogleSQL query to run, then look at the results of the query and return the answer to the input question.
     Unless the user specifies in the question a specific number of examples to obtain, query for at most 2000000 results using the LIMIT clause as per GoogleSQL. You can order the results to return the most informative data in the database.
     Never query for all columns from a table. You must query only the columns that are needed to answer the question.
-    Pay attention to use only the column names you can see in the tables below. Be careful to not query for columns that do not exist. Also, pay attention to which column is in which table.
-    Replace default grouped column names like f0 to something new to identify the new column.
+    Try to match words with similar column names. Be careful to not query for columns that do not exist. Also, pay attention to which column is in which table.
+    Replace default grouped column names like f0 to something new to identify the new column. Interpert zero or non value as 0
     
     Instructions
     1. Generate the SQL query for BigQuery by using project {project}, dataset {dataset} and table {table} from the following task:
@@ -40,6 +40,7 @@ def llm_code_gen(
     Output: 'SQL Query to Run'""",
         **parameters
     )
+    st.write(response.text)
 
     return response.text
 
@@ -55,3 +56,13 @@ if prompt:
     num_columns = df.select_dtypes(include='float64').columns
 
     st.bar_chart(df, x=object_columns[0], y=num_columns[0])
+
+
+    model = TextGenerationModel.from_pretrained("text-bison@001")
+    response=model.predict(f"""
+    You are a business analytic and your job is to get insights, recommendations and a summary 
+    from the following {df}""",
+    **parameters
+    )
+    
+    st.write(f"A though from our analyst is: {response.text}")
