9a05f4c085d4431abf0305b356835d1a34b0b33b
jchavezar <jchavezar@gmail.com>
2023-08-29 19:20:45 +0000

    aug-29-23

--- gen_ai/video/utils/store.py
+++ gen_ai/video/utils/store.py
@@ -6,11 +6,15 @@ class Client:
     def __init__(self, iterable=(), **kwargs) -> None:
         self.__dict__.update(iterable, **kwargs)
     
-    def saving_to_gcs(self, file, frame="", video=True):
+    def saving_to_gcs(self, file, video=True):
         client = storage.Client(project=self.project_id)
         if video:
             client.bucket(self.video_gcs_uri).blob(file).upload_from_filename(file)
             print("video saved on GCS job done")
         else:
-            client.bucket(self.snippets_gcs_uri).blob(file).upload_from_filename(frame)
+            if self.linux:
+                client.bucket(self.snippets_gcs_uri).blob(file.split('/')[-1]).upload_from_filename(file)
+            else:
+                client.bucket(self.snippets_gcs_uri).blob(file.split('\\')[-1]).upload_from_filename(file)
+                
 
