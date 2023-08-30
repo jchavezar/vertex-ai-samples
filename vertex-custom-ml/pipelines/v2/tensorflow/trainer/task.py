a46122223e1536e66cbaec103087d680df97cceb
sockcop <jchavezar@gmail.com>
2023-08-30 22:31:17 +0000

    aug-30-23

--- vertex-custom-ml/pipelines/v2/tensorflow/trainer/task.py
+++ vertex-custom-ml/pipelines/v2/tensorflow/trainer/task.py
@@ -14,5 +14,12 @@ model = tf.keras.models.Sequential([
   tf.keras.layers.Dense(10)
 ])
 
+loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+model.compile(optimizer='adam',
+              loss=loss_fn,
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=5)
+
 model.save(os.environ["AIP_MODEL_DIR"])
 # %%
