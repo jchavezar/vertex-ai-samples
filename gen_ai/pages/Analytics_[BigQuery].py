300ed5c2cd68c170aa2efd26a817ba0e574b4b5e
sockcop <jchavezar@gmail.com>
2023-08-31 19:23:01 +0000

    aug-31-23

--- gen_ai/pages/Analytics_[BigQuery].py
+++ gen_ai/pages/Analytics_[BigQuery].py
@@ -12,13 +12,16 @@ variables={
 }
 
 client = sockcop_vertexai.Client(variables)
+
+st.title("Analytics Code-b and BQ")
+
 res, df = client.code_bison(st.text_input("Enter some text 👇", value="Show me the max capacity by grouping per latitude and longitude"))
 df["color"]=np.random.rand(df.shape[0], 4).tolist()
 
 st.write("**Query From Code Bison:**")
 st.write(res)
 
-st.write("**Tabla con Resultados:**")
+st.write("**Response Table:**")
 st.dataframe(df)
 
 object_columns = df.select_dtypes(include='object').columns.to_list()
@@ -28,5 +31,5 @@ if len(object_columns) != 0 and len(num_columns) != 0:
     st.bar_chart(df, x=object_columns[0], y=num_columns[0])
 
 if len(num_lat_long) != 0:
-    st.write("**Mapa de Datos**")
+    st.write("**Data Map**")
     st.map(df, latitude="latitude", longitude="longitude", size=num_columns[0], color="color")
