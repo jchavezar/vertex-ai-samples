c0de29ac937696c4fbc1707dc01364d87298bff3
sockcop <jchavezar@gmail.com>
2023-09-20 17:39:23 +0000

    sep-20-23

--- pipelines-flex/tensorflow/tabular/trainer/train.py
+++ pipelines-flex/tensorflow/tabular/trainer/train.py
@@ -43,6 +43,8 @@ if __name__ == "__main__":
         azure=args.azure
         )
 
+    print(encoded_features)
+
     ################################### CREATE, COMPILE AND TRAIN MODEL #####################
 
     all_features = tf.keras.layers.concatenate(encoded_features)
