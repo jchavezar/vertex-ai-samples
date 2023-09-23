fbbd5b3265b7c8540eb69074046efd58f94e9b37
sockcop <jchavezar@gmail.com>
2023-09-23 06:20:08 +0000

    sep23-23

--- ai-flex/pytorch/tabular/variables.py
+++ ai-flex/pytorch/tabular/variables.py
@@ -14,12 +14,12 @@ prebuilt_train_image_uri_cpu_gpu = "us-docker.pkg.dev/vertex-ai/training/pytorch
 prebuilt_train_package_uri = "gs://vtxdemos-dist/ai-flex-train/trainer-pytorch.tar.gz"
 custom_predict_image_uri_cpu = "us-central1-docker.pkg.dev/vtxdemos/custom-predictions/pytorch-random_cpu:1.0"
 custom_predict_image_uri_gpu = "us-central1-docker.pkg.dev/vtxdemos/custom-predictions/pytorch-random_gpu:1.0"
-prebuilt_preidct_image_uri_cpu = "us-docker.pkg.dev/vertex-ai/prediction/pytorch-cpu.2-0:latest"
+prebuilt_predict_image_uri_cpu = "us-docker.pkg.dev/vertex-ai/prediction/pytorch-cpu.2-0:latest"
 prebuilt_predict_image_uri_gpu = "us-docker.pkg.dev/vertex-ai/prediction/pytorch-gpu.2-0:latest"
 
 # Data source and storage
 dataset_uri = "gs://vtx-datasets-public/pytorch_tabular/synthetic/train.csv"
-model_uri = f"gs://vtxdemos-models/random/{_date}"
+model_uri = f"gs://vtxdemos-models/pytorch-synthetic/{_date}"
 
 # Machine Types
 machine_type_cpu = "n1-standard-4"
