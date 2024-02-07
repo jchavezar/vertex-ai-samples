c6242d51bd11e5657df27594fe69d76473188760
jchavezar <jchavezar@gmail.com>
2024-02-07 00:32:48 -0500

    feb-7-24

---
+++ gen_ai/streamlit_website/markdowns/ask_your_doc_rag_lang.md
@@ -0,0 +1,5 @@
+## Create a table database
+
+```
+gcloud sql databases create ask_your_doc_rag_lang --instance=pg15-pgvector-demo
+```
--- gen_ai/streamlit_website/med_contex_search.py
+++ gen_ai/streamlit_website/med_contex_search.py
@@ -106,36 +106,36 @@ def app():
         st.write(matches["similarity"])
     #endregion
 
-    button = f'''<script src="https://www.gstatic.com/dialogflow-console/fast/df-messenger/prod/v1/df-messenger.js"></script>
-    <df-messenger
-      agent-id="{dialogflow_id}"
-      language-code="en">
-      <df-messenger-chat-bubble
-       chat-title="infobot"
-       bot-writing-text="..."
-       placeholder-text="Tell me something!">
-      </df-messenger-chat-bubble>
-    </df-messenger>
-    <style>
-      df-messenger {{
-        z-index: 999;
-        position: fixed;
-        bottom: 16px;
-        right: 16px;
-      }}
-    </style>'''
-
-    st.components.v1.html(button, height=700, width=350)
-
-    st.markdown(
-        """
-        <style>
-            iframe[width="350"] {
-                position: fixed;
-                bottom: 60px;
-                right: 40px;
-            }
-        </style>
-        """,
-        unsafe_allow_html=True,
-    )
+    #button = f'''<script src="https://www.gstatic.com/dialogflow-console/fast/df-messenger/prod/v1/df-messenger.js"></script>
+    #<df-messenger
+    #  agent-id="{dialogflow_id}"
+    #  language-code="en">
+    #  <df-messenger-chat-bubble
+    #   chat-title="infobot"
+    #   bot-writing-text="..."
+    #   placeholder-text="Tell me something!">
+    #  </df-messenger-chat-bubble>
+    #</df-messenger>
+    #<style>
+    #  df-messenger {{
+    #    z-index: 999;
+    #    position: fixed;
+    #    bottom: 16px;
+    #    right: 16px;
+    #  }}
+    #</style>'''
+#
+    #st.components.v1.html(button, height=700, width=350)
+#
+    #st.markdown(
+    #    """
+    #    <style>
+    #        iframe[width="350"] {
+    #            position: fixed;
+    #            bottom: 60px;
+    #            right: 40px;
+    #        }
+    #    </style>
+    #    """,
+    #    unsafe_allow_html=True,
+    #)
