c3f0468a29c8dfa0703103cff35cae7e973f8a16
jchavezar <jchavezar@gmail.com>
2024-06-27 16:47:36 -0400

    house_listing-jun27-24

---
+++ gen_ai/flet/rapid_widgets/text_to_summarize.py
@@ -0,0 +1,207 @@
+text = """
+Summarize and synthesis this email:
+
+
+
+  THE SEVEN BASIC TYPES OF CHAIN LETTERS:
+
+
+    Chain Letter Type 1:   (scroll down)
+
+    Make a wish!!!
+    Really, go on and make one!!!
+
+
+
+
+
+
+
+
+
+    Oh please.... he/she will never go out with you!!!
+
+
+
+
+
+
+
+
+
+
+    Wish something else!!!
+
+
+
+
+
+
+
+
+
+
+    Not that, you moron!!!
+
+
+
+
+
+
+
+
+
+
+    Something else!  Quick!!!
+
+
+
+
+
+
+
+
+
+
+
+    Is your finger getting tired yet?
+
+
+
+
+
+
+
+
+
+
+
+    STOP!!!!
+
+    Wasn't that fun? Hope you made a great wish.
+
+    Now, to make you feel guilty, here's what I'll do. First of all, if
+you don't send this to 5096 people in the next 5 seconds, you will be
+attacked by a mad goat and then thrown off a high building into a pile of
+garbage. It's true! Because, you know, THIS letter isn't like all of those
+fake ones, THIS one is TRUE!!
+
+    Really!!! Here's how it goes:
+
+
+    * Send this to 1 person: One person will be mad at you for sending
+them a stupid chain letter.
+    * Send this to 2-5 people: 2-5 people will be mad at you for sending
+them a stupid chain letter.
+    * 5-10 people: 5-10 people will be mad at you for sending them a stupid
+chain letter.
+    * 10-20 people: 10-20 people will be mad at you for sending them a
+stupid chain letter.
+    * 20 to 674, 951 people: 20 to 674, 951  people will be mad at you 
+for sending them a stupid chain letter.
+
+    Thanks!!!! Good Luck!!!
+
+
+
+                Chain Letter Type 2
+
+    Hello, and thank you for reading this letter.
+
+    You see, there is a starving little boy in baklaliviatatlaglooshen
+who has no arms, no legs, no parents, and no goats. This little boy's
+life could be saved, because for every time you pass this on, a dollar will
+be donated to the Little Starving Legless Armless Goatless Boy from the
+Baklaliviatatlaglooshen Fund. Remember, we have no way of counting letters
+sent and this is all bull.  So go on, reach out.
+    Send this to 5 people in the next 47 seconds.
+
+    Oh, and a reminder - if you accidentally send this to 4 or 6 people,
+you will die instantly.  Thanks again!!
+
+             Chain Letter Type 3
+
+    Hi there!! This chain letter has been in existence since 1897. This is
+absolutely incredible because there was no email then and probably not as
+many little 8 year olds writing chain letters.
+    So this is how it works. Pass this on to 15,067 people in the next 7
+minutes or something horrible will happen to you like:
+
+                Stupid Horror Story #1
+
+    Miranda Pinsley was walking home from school on Saturday. She had
+recently received this letter and ignored it. She then tripped in a crack
+in the sidewalk, fell into the sewer, was gushed down a drainpipe in a
+flood of sewerage, and went flying out over a waterfall.
+Not only did she smell nasty, she died. This Could Happen To You!!!
+
+                Stupid Horror Story #2
+
+    Dexter Bip, a 13 year old boy, got a chain letter in his mail and
+ignored it. Later that day, he was hit by a car and so was his girlfriend.
+They both died. Their families were so upset that everyone related to them
+(even by marriage) went crazy and spent the rest of
+their miserable lives in an institution. This Could Happen To You!!!
+
+                 Chain Letter Type 4:
+
+    This e-mail is wicked-cool!  It was started by Microsoft to test its
+e-mail tracking system because, you know, a big high-tech company like
+Microsoft always sends important new software out over the internet to be
+available to any moron who can operate a computer, right?  Plus, they have
+formed a secret merger with Disney Corp., who has agreed to give up
+millions of dollars in revenue by giving everyone who reads this e-mail,
+passes it on, looks at it, knows someone that looked at it, or is related
+to someone who is a friend of someone who looks at it A FREE,
+ALL-EXPENSES-PAID TRIP to Disneyland, DisneyWorld, or EuroDisney! So pass
+this on to everyone you know that is gullible enough to believe this (or
+not)!  Even if it's not true, hey - insulting all of your friends by
+implying that they are gullible by sending this to them is worth the
+improbable chance that you could go to Disneyland! Even if you lose all of
+your friends because they are tired of receiving this kind of junk from
+you, it's worth the chance, right? And just for good measure, if you don't
+send this on, Microsoft will send its specially trained attack-goats to
+pilfer your house and eat all of your family, SO SEND IT ON!!!!!
+
+                Chain Letter Type 5:
+
+
+    Here is a cute picture I drew.
+
+
+        (\          /)
+        ( \        / )
+        (  \      /  )
+         (   /<>\   )
+          ( / \/ \ )
+           /      \        __
+          (        )      (  )
+           ~~~~~~~~
+
+
+    It is a decapitated angel.  Send it on to all of your friends so it
+will brighten their day like it did yours!  If you don't, possessed goats
+will move into your house and eat all of your socks, leading you to believe
+that something is wrong with your washing machine because all of your socks
+keep disappearing.  
+    Have a nice day!!!
+
+    There. Now that we've covered and dumped on the seven main types of
+chain letters, on to the ironic part. In order for this to get any
+popularity, send it on!!! If you don't think it was funny at all, don't
+bother, but otherwise forward this sucker to everyone you know!!
+
+    If you don't, I don't care, but why not show this around? Take two
+minutes and forward it.  Thanks!
+
+    Remember, the moral of the story is, if you get a chain letter, ignore
+it. If it's a joke or something, send it, sure, but if it's gonna make
+people feel guilty (i.e. the goatless boy from Baklaliviatatlaglooshen) or
+nervous (i.e. Miranda Pinsley who ended up in a waterfall of sewerage)
+just delete it.
+
+    Do yourself a favor, and everyone else in the world, and say, "DEATH
+TO CHAIN LETTERS!!"
+
+:-)  :-)  :-)  :-)  :-)  :-)
+"""
