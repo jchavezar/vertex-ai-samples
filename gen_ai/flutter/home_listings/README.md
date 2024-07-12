9dd6d84383dbaa64ac7c8e663980a3daf3e96257
jchavezar <jchavezar@gmail.com>
2024-07-12 12:35:35 -0400

    house_listing-may23-24

--- gen_ai/flutter/home_listings/README.md
+++ gen_ai/flutter/home_listings/README.md
@@ -1,6 +1,10 @@
 # Home Listings
 
 This Flutter application showcases a powerful semantic search experience using multimodal embeddings. It leverages Google Vector Search, a robust vector database, to store and retrieve text and image embeddings, enabling users to find relevant information based on meaning rather than just keywords. The front-end is built with Flutter, providing a smooth and intuitive user interface for searching and exploring the data. This project demonstrates the potential of combining cutting-edge technologies like multimodal embeddings and vector databases to create innovative search solutions.
+
+## Diagram
+![](images/home_listings.png)
+
 ## Getting Started
 
 This project is a starting point for a Flutter application.
@@ -16,14 +20,19 @@ samples, guidance on mobile development, and a full API reference.
 
 ## Pre-requisites
 
-Run all the preprocessing layers first by following instructions in this sequence:
+Run the following notebook to create Synthetic Data & build Vector Search Indexes:
 
-1. [1_processing.ipynb](./1_processing.ipynb)
-2. [2_img_text_indexes.ipynb](./2_img_text_indexes.ipynb)
-3. [3_combined_indexes.ipynb](./3_combined_indexes.ipynb)
+1. [syntheticdata_gen_vsearch.ipynb](./syntheticdata_gen_vsearch.ipynb)
+
+***Important: "Save the following values from the previous notebook:"***
+- **combined_index_endpoint:** *e.g. projects/254356041555/locations/us-central1/indexEndpoints/2114453219189981184*
+- **combined_index_id:** *e.g. vs_abnb_deployed_image_text*
+- **text_index_endpoint** *e.g. projects/254356041555/locations/us-central1/indexEndpoints/8572615084839272448*
+- **text_index_id** *e.g. vs_abnb_deployed_text*
+- **datastore_bucket** *e.g. vtxdemos-abnb-images* In the notebook is the value of the variable "OUTPUT_IMAGE_PATH"
 
 Because Flutter needs CORS to be enable at the back and where the images are stored,
-we need to enable in the bucket.
+we need to enable it in the bucket.
 
 ```bash
 cat << EOF > cors.json
@@ -37,22 +46,27 @@ cat << EOF > cors.json
 EOF
 ```
 
-## Diagram
-![](images/home_listings.png)
-
 ```bash
-gcloud storage buckets update gs://vtxdemos-vsearch-airbnb --cors-file=cors.json
+gcloud storage buckets update gs://vtxdemos-abnb-images --cors-file=cors.json
 ```
 
 ## Step 1
 
 After cloning this repo build the middleware in your Google Cloud Project.
 
+Create the Repository
+
+```bash
+gcloud artifacts repositories create cloud-run-source-deploy --repository-format=docker \
+    --location=us-central1 --description="Docker repository"
+```
+
 *Change the region, project and **artifact registry repo**: {your_region}-docker.pkg.dev/{your_project}/{your-artifact-repository}/house_listings:middleware*
 
 ```bash
 cd middleware
-gcloud builds submit -t us-central1-docker.pkg.dev/vtxdemos/cloud-run-source-deploy/house_listings:middleware .
+export PROJECT_ID=vtxdemos
+gcloud builds submit --substitutions _TEXT_INDEX_ENDPOINT=projects/254356041555/locations/us-central1/indexEndpoints/8572615084839272448,_TEXT_INDEX_ID=vs_abnb_deployed_image_text,_COMBINED_INDEX_ENDPOINT=projects/254356041555/locations/us-central1/indexEndpoints/2114453219189981184,_COMBINED_INDEX_ID=vs_abnb_deployed_image_text,_DATASET_BUCKET=vtxdemos-abnb-images
 ```
 
 ### Deploy to Cloud Run
@@ -62,17 +76,22 @@ gcloud builds submit -t us-central1-docker.pkg.dev/vtxdemos/cloud-run-source-dep
 ```bash
 gcloud run deploy home-listings-middleware --image us-central1-docker.pkg.dev/vtxdemos/cloud-run-source-deploy/house_listings:middleware --region us-central1 --quiet --allow-unauthenticated
 ```
+
+***Important: "Save the cloud run middleware endpoint name"***
+- Go to Cloud Console > [Cloud Run](https://console.cloud.google.com/run), and look for the crun id (in this case home-listing-middleware), the endpoint should look like this: https://home-listing-middleware-oyntfgdwsq-uc.a.run.app
+
 ## Step 2
 
 Build the front end and deploy to Cloud Run. 
 
-- The build file definition [cloudbuild.yaml](https://github.com/jchavezar/vertex-ai-samples/blob/main/gen_ai/flutter/home_listings/cloudbuild.yaml) is on the repo. 
+- The build file definition [cloudbuild.yaml](https://github.com/jchavezar/vertex-ai-samples/blob/main/gen_ai/flutter/home_listings/cloudbuild.yaml) is in the repository. 
 
-*Change API_KEY from [aistudio.google.com](aistudio.google.com) or google cloud project API Keys.*
+- *Change API_KEY generated from [aistudio.google.com](aistudio.google.com) or google cloud project API Keys.*
+- *Change _MIDDLEWARE_ENDPOINT with the value saved from the previous step*
 
 ```bash
 
-gcloud builds submit --substitutions=API_KEY=''
+gcloud builds submit --substitutions API_KEY='',_MIDDLEWARE_ENDPOINT='https://home-listing-middleware-oyntfgdwsq-uc.a.run.app'
 ```
 *Change your variables as before...*
 ```bash
--- gen_ai/flutter/home_listings/middleware/main.py
+++ gen_ai/flutter/home_listings/middleware/main.py
@@ -1,5 +1,6 @@
-import base64
+import os
 import json
+import base64
 import vertexai
 import numpy as np
 import pandas as pd
@@ -10,29 +11,31 @@ from google.cloud import aiplatform
 from vertexai.vision_models import MultiModalEmbeddingModel, Image, Video
 from vertexai.language_models import TextEmbeddingInput, TextEmbeddingModel
 
-project = "vtxdemos"
+project_id = os.environ["PROJECT_ID"]
 region = "us-central1"
+combined_index_endpoint = os.environ["COMBINED_INDEX_ENDPOINT"]
+combined_index_id = os.environ["COMBINED_INDEX_ID"]
+text_index_endpoint = os.environ["TEXT_INDEX_ENDPOINT"]
+text_index_id = os.environ["TEXT_INDEX_ID"]
+datastore_bucket = os.environ["DATASET_BUCKET"]
 
-vertexai.init(project="vtxdemos", location="us-central1")
+vertexai.init(project=project_id, location=region)
 
 img_emb_model = MultiModalEmbeddingModel.from_pretrained("multimodalembedding")
 text_emb_model = TextEmbeddingModel.from_pretrained("text-embedding-004")
 
-embeddings_file = "gs://vtxdemos-vsearch-datasets/data.json"
+# Vector Search
 combined_index = aiplatform.MatchingEngineIndexEndpoint(
-    index_endpoint_name="projects/254356041555/locations/us-central1/indexEndpoints/5429850212841553920",
+    index_endpoint_name=combined_index_endpoint,
 )
 
 text_index = aiplatform.MatchingEngineIndexEndpoint(
-    index_endpoint_name="projects/254356041555/locations/us-central1/indexEndpoints/8168601736236236800"
+    index_endpoint_name=text_index_endpoint
 )
 
 app = FastAPI()
 
-app.df = pd.read_csv("gs://vtxdemos-vsearch-datasets/homes_listings/data.csv")
-app.df2 = pd.read_csv("gs://vtxdemos-vsearch-datasets/homes_listings/combined_data.csv")
-app.df3 = pd.read_pickle("gs://vtxdemos-vsearch-datasets/homes_listings/df_back.pkl")
-app.df3.loc[:, 'id'] = app.df3 .index
+app.df = pd.read_csv(f"gs://{datastore_bucket}/dataset/data.csv")
 
 origins = [
     "*"
@@ -46,99 +49,34 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-@app.post('/image')
-async def image_conversion(file: UploadFile = File(...), text_data: Optional[str] = Form(None)):
-  contents = await file.read()
-  print(text_data)
 
-  def l2_normalize(vector):
-    """Normalizes a vector to unit length using L2 normalization."""
-    l2_norm = np.linalg.norm(vector)
-    if l2_norm == 0:
-      return vector  # Avoid division by zero
-    return vector / l2_norm
-
-  def normalize_query(text_data:str, video: bytes):
-    e = img_emb_model.get_embeddings(
-        video=Video(
-            video_bytes=video
-        ),
-        contextual_text=text_data,
-    )
-    print("embeddings ready")
-
-    normalized_video_embedding = l2_normalize(e.video_embeddings[0].embedding)
-    normalized_text_embedding = l2_normalize(e.text_embedding)
-    print("normalized done")
-    we_ave =  (0.7 * normalized_video_embedding) + (0.3 * normalized_text_embedding)
-    return we_ave
-
-  if text_data:
-    e = normalize_query(text_data, contents)
-    print("it worked!")
-
-  else:
-    embeddings = img_emb_model.get_embeddings(video=Video(video_bytes=contents))
-    e = embeddings.video_embeddings[0].embedding
-
-  response = combined_index.find_neighbors(
-      deployed_index_id = "vs_abnb_deployed_image_text_1",
-      queries = [e],
-      num_neighbors = 10
-  )
-
-  print("response finished.")
-
-  nn_list = [int(i.id) for i in response[0]]
-  order_df = pd.DataFrame({'id': nn_list})
-  merged_df = app.df2.merge(order_df, on='id', how='inner')
-  final_df = merged_df.set_index('id').loc[nn_list].reset_index()
-  final_df.fillna("value", inplace = True)
-
-  res = final_df.to_json(orient="records")
-  parsed = json.loads(res)
-
-  print(final_df["Img_interior_url_0"].iloc[0])
-  print(final_df["Img_interior_url_1"].iloc[0])
-  print(final_df["Img_interior_url_2"].iloc[0])
-  print(final_df["Img_interior_url_3"].iloc[0])
-  print(final_df["Img_interior_url_4"].iloc[0])
-
-  return parsed
 
 @app.post('/text')
 async def image_conversion(text_data: Optional[str] = Form(None)):
-  embeddings = img_emb_model.get_embeddings(contextual_text=text_data).text_embedding
+  embeddings = img_emb_model.get_embeddings(
+      contextual_text=text_data).text_embedding
 
-  response = text_index.find_neighbors(
-      deployed_index_id = "vs_abnb_deployed_text_1",
-      queries = [embeddings],
-      num_neighbors = 10
+  text_response = text_index.find_neighbors(
+      deployed_index_id=text_index_id,
+      queries=[embeddings],
+      num_neighbors=10
   )
 
-  print("response finished.")
-
-  nn_list = [int(i.id) for i in response[0]]
+  nn_list = [int(i.id) for i in text_response[0]]
   order_df = pd.DataFrame({'id': nn_list})
-  merged_df = app.df2.merge(order_df, on='id', how='inner')
+  merged_df = app.df.merge(order_df, on='id', how='inner')
   final_df = merged_df.set_index('id').loc[nn_list].reset_index()
-  final_df.fillna("value", inplace = True)
+  final_df.fillna("value", inplace=True)
 
   res = final_df.to_json(orient="records")
   parsed = json.loads(res)
-
-  print(final_df["Img_interior_url_0"].iloc[0])
-  print(final_df["Img_interior_url_1"].iloc[0])
-  print(final_df["Img_interior_url_2"].iloc[0])
-  print(final_df["Img_interior_url_3"].iloc[0])
-  print(final_df["Img_interior_url_4"].iloc[0])
-
   return parsed
 
+
 @app.post('/image')
-async def image_conversion(file: UploadFile = File(...), text_data: Optional[str] = Form(None)):
+async def image_conversion(file: UploadFile = File(...),
+    text_data: Optional[str] = Form(None)):
   contents = await file.read()
-  print(contents)
 
   def l2_normalize(vector):
     """Normalizes a vector to unit length using L2 normalization."""
@@ -147,78 +85,43 @@ async def image_conversion(file: UploadFile = File(...), text_data: Optional[str
       return vector  # Avoid division by zero
     return vector / l2_norm
 
-  def normalize_query(text_data:str, video: bytes):
+  def normalize_query(text_data: str, video: bytes):
     e = img_emb_model.get_embeddings(
         video=Video(
             video_bytes=video
         ),
         contextual_text=text_data,
     )
-    print("embeddings ready")
-
     normalized_video_embedding = l2_normalize(e.video_embeddings[0].embedding)
     normalized_text_embedding = l2_normalize(e.text_embedding)
-    print("normalized done")
-    we_ave =  (0.7 * normalized_video_embedding) + (0.3 * normalized_text_embedding)
+    we_ave = (0.7 * normalized_video_embedding) + (
+        0.3 * normalized_text_embedding)
     return we_ave
 
   if text_data:
     e = normalize_query(text_data, contents)
-    print("it worked!")
 
   else:
     embeddings = img_emb_model.get_embeddings(video=Video(video_bytes=contents))
     e = embeddings.video_embeddings[0].embedding
 
-  response = combined_index.find_neighbors(
-      deployed_index_id = "vs_abnb_deployed_combined_1",
-      queries = [e],
-      num_neighbors = 10
+  combined_response = combined_index.find_neighbors(
+      deployed_index_id=combined_index_id,
+      queries=[e],
+      num_neighbors=10
   )
 
-  print("response finished.")
-
-  nn_list = [int(i.id) for i in response[0]]
+  nn_list = [int(i.id) for i in combined_response[0]]
   order_df = pd.DataFrame({'id': nn_list})
-  merged_df = app.df2.merge(order_df, on='id', how='inner')
+  merged_df = app.df.merge(order_df, on='id', how='inner')
   final_df = merged_df.set_index('id').loc[nn_list].reset_index()
-  final_df.fillna("value", inplace = True)
+  final_df.fillna("value", inplace=True)
 
   res = final_df.to_json(orient="records")
   parsed = json.loads(res)
 
-  print(final_df["Img_interior_url_0"].iloc[0])
-  print(final_df["Img_interior_url_1"].iloc[0])
-  print(final_df["Img_interior_url_2"].iloc[0])
-  print(final_df["Img_interior_url_3"].iloc[0])
-  print(final_df["Img_interior_url_4"].iloc[0])
-
   return parsed
 
-@app.post('/mlb')
-async def image_conversion(text_data: Optional[str] = Form(None)):
-  embeddings = img_emb_model.get_embeddings(contextual_text=text_data).text_embedding
-
-  response = mlb_index.find_neighbors(
-      deployed_index_id = "vs_mlb_deployed_v1",
-      queries = [embeddings],
-      num_neighbors = 10
-  )
-
-  print("response finished.")
-
-  nn_list = [int(i.id) for i in response[0]]
-  order_df = pd.DataFrame({'id': nn_list})
-  merged_df = app.df3.merge(order_df, on='id', how='inner')
-  final_df = merged_df.set_index('id').loc[nn_list].reset_index()
-  final_df.fillna("value", inplace = True)
-
-  res = final_df.to_json(orient="records")
-  parsed = json.loads(res)
-
-  print(final_df["title"].iloc[0])
-
-  return parsed
 
 if __name__ == "__main__":
   uvicorn.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
