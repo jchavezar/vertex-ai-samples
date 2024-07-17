7596d72374880c85b31c973ed106c523ecaba446
jchavezar <jchavezar@gmail.com>
2024-07-17 12:07:55 -0400

    war_of_bots_wed_17

--- gen_ai/flet/war_of_bots/back_end.py
+++ gen_ai/flet/war_of_bots/back_end.py
@@ -1,8 +1,9 @@
+import json
 import base64
 import vertexai
 from variables import *
 from openai import OpenAI
-from vertexai.generative_models import GenerativeModel, Part, FinishReason
+from vertexai.generative_models import GenerativeModel, Part, FinishReason, Tool
 import vertexai.preview.generative_models as generative_models
 import time
 
@@ -14,8 +15,54 @@ client = OpenAI(
 
 vertexai.init(project=project_id, location=location)
 
+safety_settings = {
+    generative_models.HarmCategory.HARM_CATEGORY_HATE_SPEECH: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    generative_models.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    generative_models.HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    generative_models.HarmCategory.HARM_CATEGORY_HARASSMENT: generative_models.HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+}
+
+generation_config = {
+    "max_output_tokens": 8192,
+    "temperature": 1,
+    "top_p": 0.95,
+    "response_mime_type": "application/json",
+}
+
+tools = [
+    Tool.from_google_search_retrieval(
+        google_search_retrieval=generative_models.grounding.GoogleSearchRetrieval(disable_attribution=False)
+    ),
+]
+ver_model = GenerativeModel(
+    "gemini-1.5-pro-001",
+    tools=tools,
+)
+
+def verification(prompt: str):
+
+  text1 = f'''
+  Your only task is to validate the following statement against internet and give me the % level of veracity:
+  
+  <example>
+  {{"justification": "Is tru that x is y because...", "veracity": "80%"}}
+  </example>
+
+  Query:
+  {prompt}
+  '''
+
+  response = ver_model.generate_content(
+      [text1],
+      generation_config=generation_config,
+      safety_settings=safety_settings,
+  )
+
+  return response
+
+
 def chat_gpt_4(prompt: str):
-  response = client.chat.completions.create(
+  llm_response = client.chat.completions.create(
       model="gpt-4",  # Consider using "gpt-3.5-turbo" for cost-effectiveness
       messages=[
           {"role": "user", "content": prompt},
@@ -26,7 +73,14 @@ def chat_gpt_4(prompt: str):
           """},
       ],
   )
-  return response.choices[0].message.content
+  llm_response = llm_response.choices[0].message.content
+  res = verification(prompt=llm_response)
+  response = json.loads(res.text)
+  justification = response["justification"]
+  veracity = response["veracity"]
+  citations = [i.web.uri for i in res.candidates[0].grounding_metadata.grounding_attributions]
+  return llm_response, justification, veracity, citations
+
 
 def gemini(prompt:str):
   model = GenerativeModel(
@@ -37,5 +91,11 @@ def gemini(prompt:str):
       Don't repeat yourself.
       """]
   )
-  response = model.generate_content([prompt])
-  return response.text
+  llm_response = model.generate_content([prompt])
+  llm_response = llm_response.text
+  res = verification(prompt=llm_response)
+  response = json.loads(res.text)
+  justification = response["justification"]
+  veracity = response["veracity"]
+  citations = [i.web.uri for i in res.candidates[0].grounding_metadata.grounding_attributions]
+  return llm_response, justification, veracity, citations
