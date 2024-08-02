fce585c797d2aed18e6b60bb8e8c5f2ba663bbe3
jchavezar <jchavezar@gmail.com>
2024-08-02 16:20:54 -0400

    aug-2-24

--- pipelines-flex/tensorflow/tabular/trainer/train.py
+++ pipelines-flex/tensorflow/tabular/trainer/train.py
@@ -1,8 +1,9 @@
 import os
 import warnings
 import argparse
-from trainer import preprocess
 import tensorflow as tf
+from trainer import preprocess
+from google.cloud import aiplatform
 warnings.filterwarnings('ignore')
 
 ################################### ARGUMENTS ########################################
@@ -10,30 +11,44 @@ warnings.filterwarnings('ignore')
 parser = argparse.ArgumentParser()
 parser.add_argument(
     '--dataset', 
-    help = 'dataset to train',
-    type = str
+    help='dataset to train',
+    type=str
+)
+parser.add_argument(
+    '--run_name',
+    help='name of the run',
+    type=str,
+)
+parser.add_argument(
+    '--experiment_name',
+    help='name of the experiment',
+    type=str,
+    default="ecommerce-experiment"
 )
 parser.add_argument(
     '--gcp', 
-    help = 'test to train',
-    type = bool,
-    default = True
+    help='test to train',
+    type=bool,
+    default=True
 )
 parser.add_argument(
     '--aws', 
-    help = 'test to train',
-    type = bool,
-    default = False
+    help='test to train',
+    type=bool,
+    default=False
 )
 parser.add_argument(
     '--azure', 
-    help = 'test to train',
-    type = bool,
-    default = False
+    help='test to train',
+    type=bool,
+    default=False
 )
 args = parser.parse_args()
 if __name__ == "__main__":
     ################################### FEATURE ENGINEERING #################################
+
+    aiplatform.init(experiment=args.experiment_name, project=os.environ["CLOUD_ML_PROJECT_ID"])
+    aiplatform.start_run(run=args.run_name)
 
     client = preprocess.Client()
     train_ds, val_ds, test_ds, encoded_features, all_inputs = client.transform(
@@ -57,7 +72,13 @@ if __name__ == "__main__":
 
     model.compile(optimizer='adam',
                   loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
-                  metrics=['accuracy'])
+                  metrics=[
+                      tf.keras.metrics.BinaryAccuracy(name='accuracy'),
+                      tf.keras.metrics.Precision(name='precision'),
+                      tf.keras.metrics.Recall(name='recall'),
+                      tf.keras.metrics.AUC(name='auc'),
+                      tf.keras.metrics.AUC(name='prc', curve='PR'),
+                  ])
     print('compile pass')
     ################################## SETUP TENSORBOARD LOGS AND TRAIN #####################
 
@@ -67,8 +88,24 @@ if __name__ == "__main__":
 
     #tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=os.environ['AIP_TENSORBOARD_LOG_DIR'], update_freq='batch')
     model.fit(train_ds, epochs=15, validation_data=val_ds)
-    loss, accuracy = model.evaluate(test_ds)
-    print("Accuracy", accuracy)
+    loss, accuracy, precision, recall, auc, prc = model.evaluate(test_ds)
+    print("Loss:", loss)
+    print("Accuracy:", accuracy)
+    print("Precision:", precision)
+    print("Recall:", recall)
+    print("AUC:", auc)
+    print("PRC:", prc)
+
+    aiplatform.log_metrics(
+        {
+            "loss": loss,
+            "accuracy": accuracy,
+            "precision": precision,
+            "recall": recall,
+            "auc": auc,
+            "prc": prc,
+         }
+    )
 
     ################################### SAVE MODEL ##########################################
 
