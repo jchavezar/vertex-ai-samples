8334a5e8957464c9900f6697f32edbbc58c04fcf
jchavezar <jchavezar@gmail.com>
2024-08-19 14:43:43 -0400

    aug-19-24

--- pipelines-flex/catboost/components/inference/web_container/main.py
+++ pipelines-flex/catboost/components/inference/web_container/main.py
@@ -1,20 +1,14 @@
 # %%
 import os
-import argparse
 import pandas as pd
 from google.cloud import storage
 from catboost import CatBoostClassifier
 from fastapi import Request, FastAPI
 
-parser = argparse.ArgumentParser(description='Catboost Inference')
-parser.add_argument("--project-id", type=str, default="vtxdemos")
-parser.add_argument("--aip-storage-uri")
-args = parser.parse_args()
-
 app = FastAPI()
 
-AIP_PROJECT_NUMBER = args.project_id
-AIP_STORAGE_URI = args.aip_storage_uri
+AIP_PROJECT_NUMBER = os.getenv("CLOUD_ML_PROJECT_ID")
+AIP_STORAGE_URI = os.getenv("AIP_STORAGE_URI")
 
 buck = AIP_STORAGE_URI.split("/")[2]
 blb = "/".join(AIP_STORAGE_URI.split("/")[3:])
@@ -24,7 +18,7 @@ blob = storage.Client(AIP_PROJECT_NUMBER).bucket(buck).blob(blb + "/model.json")
 blob.download_to_filename(model_file_name)
 
 cb_model: CatBoostClassifier = CatBoostClassifier()
-cb_model.load_model(model_file_name)
+cb_model.load_model(model_file_name, format="json")
 
 
 @app.get(os.getenv("AIP_HEALTH_ROUTE", "/healthcheck"), status_code=200)
---
+++ pipelines-flex/catboost/dataset.py
@@ -0,0 +1,31 @@
+from google.cloud import bigquery
+
+client = bigquery.Client(project="vtxdemos")
+
+query = f"""
+    WITH data AS (
+      SELECT *, RAND() AS random_value
+      FROM `vtxdemos.demos_us.ecommerce_balanced`
+    )
+    
+    SELECT *, 
+      CASE 
+        WHEN random_value < 0.7 THEN 'train'
+        WHEN random_value >= 0.1 AND random_value < 0.8 THEN 'test'
+        ELSE 'val' 
+      END AS split_set
+    FROM data
+    """
+
+df = client.query(query).to_dataframe()
+
+df.to_csv("gs://vtxdemos-datasets-public/ecommerce/ecommerce_balanced.csv", index=False)
+
+job = aiplatform.CustomTrainingJob(
+    display_name='test-train',
+    script_path='test_script.py',
+    requirements=['pandas', 'numpy'],
+    container_uri='gcr.io/cloud-aiplatform/training/tf-cpu.2-2:latest',
+    model_serving_container_image_uri='gcr.io/my-trainer/serving:1',
+    model_serving_container_predict_route='predict',
+    model_serving_container_health_route='metadata, labels={'key': 'value'}, )
