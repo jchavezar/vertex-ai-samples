011f4b8ac5bf0b8757100d9b8913f9b3378785a6
jchavezar <jchavezar@gmail.com>
2024-09-11 15:32:07 -0400

    wed-11-sep

--- gen_ai/flet/marketplace/first_page.py
+++ gen_ai/flet/marketplace/first_page.py
@@ -26,6 +26,23 @@ def view(page):
     items = parallel_vector_search(e.control.value)
     # items = await page.asyncio_call(vector_search_wrapper, e.control.value, e.control.value)
     for item in items:
+      print(item["uri"])
+      print(item["title"])
+      print(item["subtitle"])
+      print(item["summary"])
+      print(item["description"])
+      print(item["materials"])
+      print(item["questions"])
+      print(item["content"])
+      print(type(item["uri"]))
+      print(type(item["title"]))
+      print(type(item["subtitle"]))
+      print(type(item["summary"]))
+      print(type(item["description"]))
+      print(type(item["materials"]))
+      print(type(item["questions"]))
+      print(type(item["content"]))
+
       grid_view.controls.append(
           Column(
               alignment=MainAxisAlignment.CENTER,
@@ -34,8 +51,8 @@ def view(page):
                       content=Card(
                           elevation=20,
                           content=Container(
-                              height=120,
-                              width=120,
+                              height=150,
+                              width=150,
                               content=Image(
                                   src=item["uri"],
                                   fit=ImageFit.COVER,
@@ -60,13 +77,12 @@ def view(page):
       )
     page.update()
 
-  def add_stuff(e):
+  def listitems(e):
     grid_view.controls.clear()
     gridscreen.height = "",
     gridscreen.expand = True,
     items = list_items()
     for item in items:
-      print(item["uri"])
       grid_view.controls.append(
           Column(
               alignment=MainAxisAlignment.CENTER,
@@ -75,14 +91,25 @@ def view(page):
                       content=Card(
                           elevation=20,
                           content=Container(
-                              height=120,
-                              width=120,
+                              height=150,
+                              width=150,
                               content=Image(
                                   src=item["uri"],
                                   fit=ImageFit.COVER,
                               )
                           )
-                      )
+                      ),
+                      data={
+                          "uri": item["uri"],
+                          "title": item["title"],
+                          "subtitle": item["subtitle"],
+                          "summary": item["summary"],
+                          "description": item["description"],
+                          "materials": item["materials"],
+                          "questions": item["questions"],
+                          "content": item["content"]
+                      },
+                      on_click=navigate_to_search_page
                   ),
                   Text(item["title"])
               ]
@@ -92,11 +119,11 @@ def view(page):
 
   grid_view: GridView = GridView(
       # expand=True,
-      runs_count=5,
+      # runs_count=5,
       max_extent=200,
       # child_aspect_ratio=1,
-      spacing=15,
-      run_spacing=15,
+      spacing=100,
+      run_spacing=1,
   )
 
   gridscreen = Container(
@@ -127,59 +154,64 @@ def view(page):
           Column(
               # alignment=MainAxisAlignment.CENTER,
               controls=[
-                  Divider(height=20, color=colors.TRANSPARENT),
-                  Container(
-                      height=50,
-                      padding=padding.only(left=15, right=15),
-                      margin=margin.only(left=150, right=150),
-                      border_radius=30,
-                      border=border.all(2, colors.BLACK),
-                      bgcolor=colors.WHITE,
-                      content=Row(
-                          controls=[
-                              TextField(
-                                  hint_text="Search for anything",
-                                  hint_style=TextStyle(color=colors.GREY_500, font_family="Helvetica", weight=FontWeight.W_200),
-                                  expand=True,
-                                  border_color=colors.TRANSPARENT,
-                                  data={"uri": "test"},
-                                  on_submit=search
-                              ),
-                              Icon(
-                                  name=icons.SEARCH,
-                                  color=colors.ORANGE,
-                                  size=25,
-                              ),
-                          ]
-                      )
-                  ),
-                  # Container(
-                  #     bgcolor=colors.GREEN_50,
-                  #     height=300,
-                  #     content=main_layout
-                  # ),
-                  main_layout,
-                  Container(
-                      margin=margin.only(left=50),
-                      height=100,
-                      content=Row(
-                          alignment=MainAxisAlignment.START,
-                          controls=[
-                              ElevatedButton(
-                                  "List Items",
-                                  on_click=add_stuff
-                              ),
-                              ElevatedButton(
-                                  "Solution Diagram",
-                                  on_click=navigate_to_second_page,
-                                  bgcolor=colors.BLUE,
-                                  color=colors.WHITE,
+                  Divider(height=10, color=colors.TRANSPARENT),
+                  Row(
+                      alignment=MainAxisAlignment.SPACE_EVENLY,
+                      controls=[
+                          Container(
+                              margin=margin.only(left=50),
+                              height=75,
+                              width=75,
+                              content=Image(src="https://gcpetsy.sonrobots.net/artifacts/etsy.png", fit=ImageFit.CONTAIN)
+                          ),
+                          Container(
+                              height=40,
+                              expand=True,
+                              padding=padding.only(left=5, right=5),
+                              margin=margin.only(left=15, right=75),
+                              border_radius=30,
+                              border=border.all(2, colors.BLACK),
+                              bgcolor=colors.WHITE,
+                              content=Row(
+                                  alignment=MainAxisAlignment.SPACE_BETWEEN,
+                                  controls=[
+                                      TextField(
+                                          hint_text="Search for anything",
+                                          hint_style=TextStyle(color=colors.GREY_500, font_family="Helvetica", weight=FontWeight.W_200, size=14),
+                                          # expand=True,
+                                          border_color=colors.TRANSPARENT,
+                                          data={"uri": "test"},
+                                          on_submit=search
+                                      ),
+                                      Icon(
+                                          name=icons.SEARCH,
+                                          color=colors.DEEP_ORANGE_400,
+                                          size=25,
+                                      ),
+                                  ]
                               )
-                          ]
-                      ),
+                          ),
+                      ]
                   ),
-
+                  main_layout,
               ]
+          ),
+          BottomAppBar(
+              bgcolor=colors.TRANSPARENT,
+              content=Row(
+                  controls=[
+                      IconButton(
+                          icon=icons.PHOTO_LIBRARY,
+                          on_click=listitems,
+                          icon_color=colors.DEEP_ORANGE_400
+                      ),
+                      IconButton(
+                          icon=icons.ARCHITECTURE,
+                          on_click=navigate_to_second_page,
+                          icon_color=colors.DEEP_ORANGE_400
+                      ),
+                  ]
+              )
           )
       ]
   )
