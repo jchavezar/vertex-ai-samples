d3c4019e88a1dfa96369228740835a65399b2548
jchavezar <jchavezar@gmail.com>
2024-10-23 12:43:28 -0400

    flutt oct 21 24

--- gen_ai/flutter/marketplace_ai/lib/search_results.dart
+++ gen_ai/flutter/marketplace_ai/lib/search_results.dart
@@ -307,7 +307,7 @@ class _ListingIdState extends State<ListingId> {
               i++)
                 ElevatedButton(
                   onPressed: () async {
-                    var request = http.MultipartRequest('POST', Uri.parse("http://localhost:8000/vais"), );
+                    var request = http.MultipartRequest('POST', Uri.parse("https://markeplace-basic-254356041555.us-east1.run.app/vais"), );
                     request.fields['text_data'] = widget.dataset["questions_only_cat3"][i];
                     var streamedResponse = await request.send();
 
