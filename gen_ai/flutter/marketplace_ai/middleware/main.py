40acd978904e6a7e0c626aba17bedb76b05d327d
jchavezar <jchavezar@gmail.com>
2024-10-31 15:58:59 -0400

    flutt oct 28 24

--- gen_ai/flutter/marketplace_ai/middleware/main.py
+++ gen_ai/flutter/marketplace_ai/middleware/main.py
@@ -52,7 +52,6 @@ serving_config = f"projects/{project_id}/locations/global/collections/default_co
 vertexai.init(project=project_id, location=region)
 client = discoveryengine.SearchServiceClient()
 
-
 @app.get("/healthz")
 async def healthz():
   return {"status": "ok"}
