d71620f212e5faa9c83370288d720c4f5cb7fd5a
jchavezar <jchavezar@gmail.com>
2024-11-07 20:36:13 -0500

    nov-7-24

--- gen_ai/gemini_tools/youtube_multipart/main.py
+++ gen_ai/gemini_tools/youtube_multipart/main.py
@@ -1,15 +1,18 @@
+import time
 import asyncio
 from sockcop_vertexai import Model
 
 model = Model(
     project="vtxdemos",
     location="us-west1",
-    model="gemini-1.5-flash-001"
+    model="gemini-1.5-flash-002"
 )
 
+start_time = time.time()
 results = asyncio.run(model.generate_content(  # Direct call
-    prompt="whats happening in this frame? how many seconds are?",
+    prompt="Give me a description of the following clip",
     yt_link="https://www.youtube.com/watch?v=sydxML4wIKQ",
-    video_length_to_process=40 # total seconds
+    video_length_to_process=40
 ))
 print(results)
+print(time.time() - start_time)
