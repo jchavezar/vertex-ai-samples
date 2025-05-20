d29dc26dc59fe2ce88805e3167577012325849b5
jesusarguelles <jesusarguelles@google.com>
2025-05-20 08:11:06 +0200

    adk may 20

---
+++ gen_ai/adk/sub_agents/database.py
@@ -0,0 +1,148 @@
+illya_doc = """
+Profile of Illya Naumov
+Since joining Google in early 2022, I've had the pleasure of partnering with cutting-edge financial services and capital market clients across New York City and the globe. My expertise spans the entire GCP product portfolio, and I've had the satisfaction of supporting a diverse range of clients engaged in Infrastructure Modernization, High-Performance Computing, Data Analytics, and ML/AI endeavors.
+
+Prior to my time at Google, I spent five formative years in Storage Infrastructure, Enterprise Backup, and Disaster Recovery. This experience laid a solid foundation for my subsequent role at Amazon Web Services (AWS), where I further deepened my cloud knowledge and gained valuable insights into a broader array of customer issues. 
+
+I'm genuinely passionate about empowering businesses to achieve their objectives through the transformative power of cloud technology. I'm always keen to connect with fellow industry professionals, engage in stimulating discussions, and share my expertise on all things GCP.
+
+Feel free to reach out if you're interested in scheduling a chat, exploring cloud solutions, or simply learning more about Google and GCP. I'm always happy to connect and share my insights.
+
+Customer Engineer, Financial Services
+Customer Engineer, Financial Services
+Google · Full-timeGoogle · Full-time
+Mar 2022 - Present · 3 yrs 2 mosMar 2022 to Present · 3 yrs 2 mos
+New York, New York, United StatesNew York, New York, United States
+I am adept at delivering exceptional business value to Financial Services Industries (FSI) clients by leveraging Google Cloud's infrastructure modernization capabilities. Partnering closely with clients, I thoroughly assess their unique needs and craft customized solutions that foster growth, enhance efficiency, and spark innovation.
+
+Spearheading customer engagements and delivering successful proofs of concept (POCs), I harness Google Cloud's cutting-edge technology to tackle FSI clients' specific challenges head-on. I proactively engage with business leaders and C-suite executives to gain a deep understanding of their strategic objectives and present tailored solutions that align seamlessly with their goals.
+
+Empowering clients to uncover innovative opportunities, optimize processes, and achieve sustainable growth is at the heart of my approach. Leveraging the full spectrum of GCP's capabilities, I help clients navigate the path to success, transforming their businesses for the future.I am adept at delivering exceptional business value to Financial Services Industries (FSI) clients by leveraging Google Cloud's infrastructure modernization capabilities. Partnering closely with clients, I thoroughly assess their unique needs and craft customized solutions that foster growth, enhance efficiency, and spark innovation. Spearheading customer engagements and delivering successful proofs of concept (POCs), I harness Google Cloud's cutting-edge technology to tackle FSI clients' specific challenges head-on. I proactively engage with business leaders and C-suite executives to gain a deep understanding of their strategic objectives and present tailored solutions that align seamlessly with their goals. Empowering clients to uncover innovative opportunities, optimize processes, and achieve sustainable growth is at the heart of my approach. Leveraging the full spectrum of GCP's capabilities, I help clients navigate the path to success, transforming their businesses for the future.…see more
+Infrastructure Modernization, Product Demonstration and +10 skills
+
+Companies Illya Worked:
+Amazon Web Services (AWS) logo
+Cloud Support Engineer II
+Cloud Support Engineer II
+Amazon Web Services (AWS) · Full-timeAmazon Web Services (AWS) · Full-time
+Jan 2021 - Mar 2022 · 1 yr 3 mosJan 2021 to Mar 2022 · 1 yr 3 mos
+Seattle, Washington, United StatesSeattle, Washington, United States
+ helped me get this job
+LinkedIn helped me get this job
+• Applied advanced troubleshooting techniques to deliver unique solutions to customers, resolving 40 complex cases per month.
+
+
+Storage and Backup Engineer
+Storage and Backup Engineer
+EpicEpic
+Nov 2018 - Dec 2020 · 2 yrs 2 mosNov 2018 to Dec 2020 · 2 yrs 2 mos
+Madison, Wisconsin AreaMadison, Wisconsin Area
+• Worked with Pure, EMC, and Hitachi storage arrays, delivering SAN storage to a large multi-tenant environment.
+
+Blue Cross Blue Shield of Michigan logo
+Blue Cross Blue Shield of Michigan
+Blue Cross Blue Shield of Michigan
+2 yrs 8 mos2 yrs 8 mos
+Systems Administrator I
+Systems Administrator I
+Oct 2018 - Nov 2018 · 2 mosOct 2018 to Nov 2018 · 2 mos
+Detroit, MichiganDetroit, Michigan
+· Backup and Recovery lead in 6PB environment consisting of EMC and Cohesity products. 
+· Managed NetApp NAS filers (7-Mode and cDOT) across 17 local and satellite sites. 
+· Developed advanced reporting utilizing EMC DPA and custom scripting. 
+· Utilized scripting languages (Python, PowerShell, Bash) to automate workflows.· Backup and Recovery lead in 6PB environment consisting of EMC and Cohesity products. · Managed NetApp NAS filers (7-Mode and cDOT) across 17 local and satellite sites. · Developed advanced reporting utilizing EMC DPA and custom scripting. · Utilized scripting languages (Python, PowerShell, Bash) to automate workflows.…see more
+Storage Systems Analyst Associate
+Storage Systems Analyst Associate
+Apr 2017 - Nov 2018 · 1 yr 8 mosApr 2017 to Nov 2018 · 1 yr 8 mos
+Detroit, MichiganDetroit, Michigan
+· Administrator for backup network consisting of Avamar and Data Domain hardware
+· Administrator for NAS storage utilizing NetApp and EMC products
+· Develop scripting solutions for system adminstration tasks
+· Create specifications and evaluate potential storage and backup systems
+· Assist email discovery searches and develop compliance processes for the legal and HR departments
+· Implement EMC Data Protection Adviser for backup monitoring and reporting
+· Assisted migration of Avamar environment to newest hardware and code level
+· Developed concise retention policies to maximize existing storage capacity
+· Developed solutions to support legacy environment backups on new hardware· Administrator for backup network consisting of Avamar and Data Domain hardware · Administrator for NAS storage utilizing NetApp and EMC products · Develop scripting solutions for system adminstration tasks · Create specifications and evaluate potential storage and backup systems · Assist email discovery searches and develop compliance processes for the legal and HR departments · Implement EMC Data Protection Adviser for backup monitoring and reporting · Assisted migration of Avamar environment to newest hardware and code level · Developed concise retention policies to maximize existing storage capacity · Developed solutions to support legacy environment backups on new hardware…see more
+Storage Administrator Intern
+Storage Administrator Intern
+Apr 2016 - Apr 2017 · 1 yr 1 moApr 2016 to Apr 2017 · 1 yr 1 mo
+Detroit, MIDetroit, MI
+· Received hands on training for a variety of EMC and NetApp hardware
+· Led intern Project, Blues Go Green, to develop new methods for saving paper and other company resources
+"""
+
+luis_doc = """
+Profile of Luis Lopez
+View Luis Lopez’s profile 
+
+• 1st1st degree connection
+Generative AI at Google | Author | Ex-AWS & Stripe
+New York, NY
+
+Companies Luis Worked:
+Google
+Google
+1 yr 8 mos1 yr 8 mos
+New York City Metropolitan AreaNew York City Metropolitan Area
+Product Manager - BigQuery ML
+Product Manager - BigQuery ML
+Part-timePart-time
+Jan 2025 - Present · 4 mosJan 2025 to Present · 4 mos
+Currently dedicating 20% of my time to a project focused on developing a new framework for simplifying technical assets discovery, product feature requests, and unlocking new revenue opportunities across BQML, aiming to make advanced analytics more accessible to all users. This initiative allows me to explore innovative approaches and contribute to the future evolution of BigQuery ML.
+
+
+Senior Solutions Architect - Startups
+Senior Solutions Architect - Startups
+Amazon Web Services (AWS) · Full-timeAmazon Web Services (AWS) · Full-time
+Mar 2023 - Sep 2023 · 7 mosMar 2023 to Sep 2023 · 7 mos
+Working with Startups to build and scale their products and businesses with AWS
+
+Principal Partner SA - GSI Leader
+Principal Partner SA - GSI Leader
+Stripe · Full-timeStripe · Full-time
+May 2022 - Nov 2022 · 7 mosMay 2022 to Nov 2022 · 7 mos
+New York CityNew York City
+Worked with customers and partners to help them adopt Stripe at scale across their organizations. 
+Responsible of Stripe’s Global System Integrators (GSI) partners technical relationship; Accenture, Deloitte, PwC, and Capgemini.
+Authored Stripe’s technical strategy across GSI partners. This includes motions to accelerate partner internal enablement, how to co-deliver customer implementations across regions, and new mechanisms to create close collaborations with adjacent Stripe’s technical teams such as Service Teams, Proserve, and Training & Certification.
+Collaborated with internal and external product teams to align roadmaps between Stripe services and GSI vertical solutions; Deloitte’s Converge Suite and Nimble, Accenture REACH and CIP solutions, PwC Banking Suite
+Worked closely with GSI leaders across verticalized practices to define co-marketing, GTM, and post-implementation strategy. Increased awareness and utilization of Stripe services across Payments, Insurance, and Banking practices.
+
+Amazon Web Services (AWS)
+Amazon Web Services (AWS)
+Full-time · 4 yrs 6 mosFull-time · 4 yrs 6 mos
+Senior AI/ML Specialist Solutions Architect
+Senior AI/ML Specialist Solutions Architect
+Jun 2021 - May 2022 · 1 yrJun 2021 to May 2022 · 1 yr
+Working with customers across various industries and geographies on Machine Learning based recommender systems. 
+My responsibilities include:
+Advising customers on how to best leverage User-Item Interactions data to create custom models using Amazon Personalize to solve for personalized recommendations (online/batch), items re-ranking, and similar items recommendations.
+Create curated content to showcase AWS’s platform AI/ML capabilities, architectural best practices, and sample solutions.Working with customers across various industries and geographies on Machine Learning based recommender systems. My responsibilities include: Advising customers on how to best leverage User-Item Interactions data to create custom models using Amazon Personalize to solve for personalized recommendations (online/batch), items re-ranking, and similar items recommendations. Create curated content to showcase AWS’s platform AI/ML capabilities, architectural best practices, and sample solutions.…see more
+AI/ML Specialist Solutions Architect
+AI/ML Specialist Solutions Architect
+May 2020 - Jun 2021 · 1 yr 2 mosMay 2020 to Jun 2021 · 1 yr 2 mos
+New York City Metropolitan AreaNew York City Metropolitan Area
+Working with customers across various industries and geographies on Machine Learning based recommender systems.Working with customers across various industries and geographies on Machine Learning based recommender systems.
+Partner Solutions Architect - GSI Group
+Partner Solutions Architect - GSI Group
+Dec 2017 - May 2020 · 2 yrs 6 mosDec 2017 to May 2020 · 2 yrs 6 mos
+New York, New YorkNew York, New York
+Worked with Global System Integrators (GSI) Partners across various industries and projects. My main focus in this role was to provide architectural guidance to GSI Partners on topics such as Serverless, Machine Learning, AR/VR, Data Engineering, IVR systems, Application Development, and DevOps.
+
+Software Engineer
+Software Engineer
+Blue Cross and Blue Shield of MinnesotaBlue Cross and Blue Shield of Minnesota
+May 2016 - Nov 2017 · 1 yr 7 mosMay 2016 to Nov 2017 · 1 yr 7 mos
+Greater Minneapolis-St. Paul AreaGreater Minneapolis-St. Paul Area
+Development of an AWS Lex bot used to aid our Service Desk team. The bot identifies a user and opens a call record or a self-service incident in ServiceNow using Amazon Connect. This has a direct impact in the amount of time a service desk agent spends per call. 
+
+Developed web applications using Angular, Amazon Cognito, S3, Cloud Front, API Endpoints, and Lambda Functions for the backend. These pages vary from admin portals to static content pages.
+
+Constant development of micro services using Lambda Functions for our IOT and Chat Bots solutions
+
+Contributed to the development of Alexa skills connected to ServiceNow used to create self-service incidents, get information, and request assistance from the Service Desk team
+
+Developed a mobile application using NativeScript (Angular 2 and TypeScript) used for our internal Fitness Solution
+
+"""
