2a4be5fa8c7164f00adf01c898af76719139d03e
jesusarguelles <jesusarguelles@google.com>
2025-05-20 08:06:01 +0200

    vais agentic may 20 (update)

--- gen_ai/google_ai_applications/datastores_unstructured_data/agent.py
+++ gen_ai/google_ai_applications/datastores_unstructured_data/agent.py
@@ -1,10 +1,12 @@
 import json
-
 import vertexai
+from google.genai import types
 from google.adk.agents import Agent
+from google.adk.planners import BuiltInPlanner
 from google.protobuf.json_format import MessageToDict
 from google.api_core.client_options import ClientOptions
 from google.cloud import discoveryengine_v1 as discoveryengine
+from datastores_unstructured_data.prompts import system_instruction
 import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -27,8 +29,11 @@ def retrieval(prompt: str, category: str):
     :param category: category to filter.
     :return: str
     """
-    print(prompt)
-    print(category)
+    logger.info("[Retrieval] Starting")
+    logger.info("-"*80)
+    logger.info(f"Category:\n {category}")
+    logger.info(f"Prompt:\n {prompt}")
+    logger.info("-"*80)
     request = discoveryengine.SearchRequest(
         query=prompt,
         filter=f"category: ANY(\"{category}\")",
@@ -50,65 +55,6 @@ def retrieval(prompt: str, category: str):
 
     markdown_output = []
 
-    # for response in page_result:
-    #     document = response.document
-    #
-    #     markdown_output.append(f"## Document ID: {document.id}")
-    #     markdown_output.append(f"**Title:** {document.struct_data['title']}")
-    #     markdown_output.append(f"**Link:** {document.derived_struct_data['link']}")
-    #
-    #     if document.derived_struct_data and 'extractive_segments' in document.derived_struct_data:
-    #         extractive_segments_list = document.derived_struct_data['extractive_segments']
-    #         markdown_output.append("\n### Extracted Segments:")
-    #         for i, segment in enumerate(extractive_segments_list):
-    #             markdown_output.append(f"#### Segment {i+1}:")
-    #
-    #             if "content" in segment:
-    #                 markdown_output.append(f"**Content:**\n```text\n{segment['content']}\n```")
-    #             else:
-    #                 markdown_output.append("**Content:** Not available")
-    #
-    #             if "relevanceScore" in segment:
-    #                 markdown_output.append(f"**Relevance Score:** {segment['relevanceScore']}")
-    #             else:
-    #                 markdown_output.append("**Relevance Score:** Not available")
-    #
-    #             if "page_span" in segment and segment["page_span"]:
-    #                 page_start = segment["page_span"].get("page_start", 0)
-    #                 page_end = segment["page_span"].get("page_end", 0)
-    #                 markdown_output.append(f"**Page Span:** {int(page_start)} - {int(page_end)}")
-    #             else:
-    #                 markdown_output.append("**Page Span:** Not available")
-    #
-    #             if "id" in segment:
-    #                 markdown_output.append(f"**Segment ID:** {segment['id']}")
-    #             else:
-    #                 markdown_output.append("**Segment ID:** Not available")
-    #
-    #             # Accessing previous and next segments
-    #             if "previous_segments" in segment and segment["previous_segments"]:
-    #                 markdown_output.append("**Previous Segments:**")
-    #                 for j, ps in enumerate(segment["previous_segments"]):
-    #                     if "content" in ps:
-    #                         markdown_output.append(f"  - **Prev Segment {j+1} Content:**\n```text\n{ps['content']}\n```")
-    #                     else:
-    #                         markdown_output.append(f"  - **Prev Segment {j+1} Content:** Not available")
-    #
-    #             if "next_segments" in segment and segment["next_segments"]:
-    #                 markdown_output.append("**Next Segments:**")
-    #                 for j, ns in enumerate(segment["next_segments"]):
-    #                     if "content" in ns:
-    #                         markdown_output.append(f"  - **Next Segment {j+1} Content:**\n```text\n{ns['content']}\n```")
-    #                     else:
-    #                         markdown_output.append(f"  - **Next Segment {j+1} Content:** Not available")
-    #
-    #             markdown_output.append("---")
-    #
-    #     if response.model_scores and 'relevance_score' in response.model_scores:
-    #         markdown_output.append(f"**Document Relevance Score:** {response.model_scores['relevance_score'].values[0]}")
-    #
-    #     markdown_output.append("=" * 50)
-
     documents = [MessageToDict(i.document._pb) for i in page_result]
 
     parsed_doc = []
@@ -125,28 +71,30 @@ def retrieval(prompt: str, category: str):
         parsed_doc.append(
             {"doc_id": content["name"], "content": content_list, "annotations": annotations_list}
         )
-    print(parsed_doc)
-    for i in parsed_doc:
-        print(i["doc_id"])
-        print("-"*80)
-        print("annotations")
-        print(i["annotations"])
     re = json.dumps(parsed_doc)
     logger.info(f"category:\n {category}")
-    logger.info(f"Markdown:\n {re}")
+    logger.info(f"Retrieval Chunks:\n {re}")
+    logger.info("-"*80)
 
     return re
+
+my_planner = BuiltInPlanner(
+    thinking_config=types.ThinkingConfig(
+        thinking_budget=0
+    )
+)
 
 root_agent = Agent(
     name="expert_agent",
     model="gemini-2.0-flash-001",
     description="You are an Agent with local grounding ready to answer any question.",
+    global_instruction=system_instruction,
     instruction="""
     Use your tool to extract any relevant information to answer your question:
     Instructions:
     Your tools needs a prompt/query to extract the information and category (network, management, marketing, code) pick
     one based on the intent.
-    Give me the details of your grounding in the response.
+    Give me the details of your grounding in the response. And a brief Explanation of your conclusions
     """,
-    tools=[retrieval]
+    tools=[retrieval],
 )
