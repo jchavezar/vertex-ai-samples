89d3a8eaff868eaeb1bab65c8ec265af33b98055
Jesus Chavez <jchavezar@gmail.com>
2025-08-02 12:32:51 -0400

    file upload for excel upload, Aug 1 25

--- gen_ai/adk/agentspace_adk_excel/to_agentspace.py
+++ gen_ai/adk/agentspace_adk_excel/to_agentspace.py
@@ -9,8 +9,7 @@ from vertexai import agent_engines
 import google.auth.transport.requests
 from vertexai.preview import reasoning_engines
 
-#%%
-as_app = "agentspace-1752271881637_1752271881637"
+as_app = "agentspace-testing_1748446185255"
 project = os.getenv("GOOGLE_CLOUD_PROJECT")
 location = os.getenv("GOOGLE_CLOUD_LOCATION")
 staging_bucket = "gs://vtxdemos-staging"
@@ -18,6 +17,7 @@ agent_display_name = "Upload Excel"
 agent_description = "Use the agent to answer any question"
 
 vertexai.init(project=project, staging_bucket=staging_bucket)
+#%%
 
 app = reasoning_engines.AdkApp(
     agent=root_agent,
@@ -28,10 +28,12 @@ remote_app = agent_engines.create(
     agent_engine=app,
     requirements=[
         "google-cloud-aiplatform[adk,agent_engines]",
-        "pandas"
+        "pandas",
+        "openpyxl",
+        "tabulate"
     ],
     extra_packages=[
-        "agent.py",
+        "agent.py"
     ],
     env_vars={
     }
@@ -66,16 +68,20 @@ payload = {
     "displayName": agent_display_name,
     "description": agent_description,
     "icon": {
-        "uri": "https://symbols.getvecta.com/stencil_4/12_google-cloud-armor.566a831ff9.svg"
+        "uri": "https://static.vecteezy.com/system/resources/previews/021/344/211/non_2x/file-upload-icon-for-your-website-mobile-presentation-and-logo-design-free-vector.jpg"
     },
     "adk_agent_definition": {
         "tool_settings": {
             "tool_description": "You are a proxy only, ask for a document and use your tool"
         },
         "provisioned_reasoning_engine": {
-            "reasoning_engine": remote_app.resource_name
-        }
-    }
+            # "reasoning_engine": remote_app.resource_name
+            "reasoning_engine": "projects/254356041555/locations/us-central1/reasoningEngines/690554874894483456",
+        },
+        "authorizations": [
+            f"projects/{project}/locations/global/authorizations/confluenceauth"
+        ]
+}
 }
 
 headers = {
@@ -87,3 +93,26 @@ headers = {
 
 response = requests.post(api_url, headers=headers, data=json.dumps(payload))
 
+
+# Update Agent Engine [Optional]
+#%%
+
+app = reasoning_engines.AdkApp(
+    agent=root_agent,
+    enable_tracing=True,
+)
+
+deployed_agent = agent_engines.get("projects/254356041555/locations/us-central1/reasoningEngines/2658311252706590720")
+# noinspection PyTypeChecker
+deployed_agent.update(
+    agent_engine=app,
+    requirements=[
+        "google-cloud-aiplatform[adk,agent_engines]",
+        "pandas",
+        "openpyxl",
+        "tabulate"
+    ],
+    extra_packages=[
+        "agent.py",
+    ],
+)
